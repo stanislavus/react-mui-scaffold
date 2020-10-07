6ecb06e0506062eb37979aaf7a648f07d154f1bc
Stanislav Usoltsev <stanislav.usoltsev@gmail.com>
2020-10-07 14:53:03 +0300

    Allow synthetic default imports

--- src/SimpleComponent.tsx
+++ src/SimpleComponent.tsx
@@ -1,5 +1,15 @@
-import * as React from 'react';
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+    a: number,
+    b: number,
+}
+
+const Test: FunctionComponent<Props> = ({ a, b }: Props) => <div>{a + b}</div>;
 
 export default () => (
-  <div>Hello world from TypeScript</div>
+  <>
+    <div>Hello world from TypeScript</div>
+    <Test a={1} b={2} />
+  </>
 );
