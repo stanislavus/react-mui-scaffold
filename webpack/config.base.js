2955cb1fe94d49239c250f037b73dc1891f8e411
Stanislav Usoltsev <stanislav.usoltsev@gmail.com>
2020-10-21 18:38:10 +0300

    Update style loading

--- webpack/config.base.js
+++ webpack/config.base.js
@@ -61,6 +61,22 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      // Styles: Inject CSS into the head with source maps
+      {
+        test: /\.(s[ac]ss|css)$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { sourceMap: true, importLoaders: 1 } },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+
+      // Images: Copy image files to build folder
+      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+
+      // Fonts and SVGs: Inline files
+      { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
     ],
   },
 };
